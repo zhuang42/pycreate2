8df3d1d7926a658cdaa3134be73287a31fcd5945
zhuang42 <zichun.zhuang@mail.utoronto.ca>
2020-07-03 16:41:42 -0400

    A simple setup.py added

---
+++ pycreate2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="pycreate2", # Replace with your own username
+    version="4.0.1",
+    author="Zichun",
+    author_email="author@example.com",
+    description="pycreate2",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
